657df0c098b563905dbf2af00f41054ef8e23586
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-03 15:14:58 -0300

    feat: add class mudar canal

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -2,8 +2,8 @@ public class SmartTv {
 
     boolean ligada = true;
     int canal = 1;
-    int volume = 25;   
-    
+    int volume = 25; 
+        
     public void aumentarCanal(){
         canal++;
     }
@@ -12,7 +12,9 @@ public class SmartTv {
         canal--;
     }
     
-    
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
 
     public void aumentarVolume(){
         volume++;
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Usuario {
     public static void main(String[] args) throws Exception {
+        int novoCanal;
         SmartTv smartTv = new SmartTv();
         Scanner teclado = new Scanner(System.in);
 
@@ -14,6 +15,9 @@ public class Usuario {
 
         smartTv.aumentarVolume();
         System.out.println("Volume atual?" + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual?" + smartTv.canal);
 
     }
 }
