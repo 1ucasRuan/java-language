ad3e627d30e8d064b1ab4f322ebcb730f407546a
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-03 14:54:27 -0300

    doc: add exemplo TV SMART

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,15 @@
+public class SmartTv {    
+
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 25;   
+        
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    } 
+        
+    
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Tv está ligada?" + smartTv.ligada);
+        System.out.println("Canal atual?" + smartTv.canal);
+        System.out.println("Volume atual?" + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Tv está ligada?" + smartTv.ligada);
+
+    }
+}
