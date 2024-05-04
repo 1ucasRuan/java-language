12ebcae24761c2db5a13c2624c882a3a8f4a8897
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-04 12:56:46 -0300

    feat: close scanner ao finalizar a execução do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -23,7 +23,7 @@ public class ContaTerminal {
 
         System.out.println("Olá " + nomeCliente +  " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " "+ numero + " e seu saldo " + saldo + " já está disponível para saque");
 
-        
+        teclado.close();
         
 
     }
