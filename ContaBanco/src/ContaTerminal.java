759f3e01f84364f216acb8f5544348aded12ac49
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-04 12:55:00 -0300

    doc: add ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+
+        int numero; 
+        String agencia; 
+        String nomeCliente; 
+        Double saldo;
+        
+        System.out.print("Informe seu nome: ");
+        nomeCliente = teclado.nextLine();
+
+        System.out.print("Informe a agencia: ");
+        agencia = teclado.nextLine();
+
+        System.out.print("Informe o numero da conta: ");
+        numero = teclado.nextInt();
+
+        System.out.print("Informe seu saldo: ");
+        saldo = teclado.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +  " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " "+ numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        
+        
+
+    }
+}
