8d1991e2b8ecf95a0543b6ac2a53f334d277a9fb
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-09 00:57:44 -0300

    feat: add menu conta bancaria

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void menuInicio() {
-        System.out.print("Inicio");
+       String menu = "Inicio";
+       System.out.println(menu);
     }
 
     public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in); 
-        double saldo = 0; 
-        // Loop infinito para manter o programa em execução até que o usuário decida sair
-        
-        while (true) { 
-            String menu = menuInicio();
-            int opcao = scanner.nextInt(); 
+        Scanner teclado = new Scanner(System.in);
+        double saldo = 0;
+        // Loop infinito para manter o programa em execução até que o usuário decida
+        // sair
+
+        while (true) {
+            menuInicio();
+            int opcao = teclado.nextInt();
             // TODO: Implemente as condições necessárias para avaliaa a opção escolhida:
-            // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
-            
-        
-        
-            // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
-        default:
-            System.out.println("Opção inválida. Tente novamente."); 
+            // Dica: Utilze o switch/case para o programa realizar as operações escolhidas
+            // pelo usuário.
+
+            // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:
+
         }
-        teclado.close();
+        //teclado.close();
 
-    // implementar orientação a objetos;
-    // implementar listas ou matrizes;
+        // implementar orientação a objetos;
+        // implementar listas ou matrizes;
 
-}}
+    }
+}
